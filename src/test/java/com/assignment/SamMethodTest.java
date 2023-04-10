7d62c19a250ca567e83b46c733bd037b2c3c7c6d
imanasmd <imanasmd@gmail.com>
2023-04-10 16:52:14 +0530

    maven_project

---
+++ src/test/java/com/assignment/SamMethodTest.java
@@ -0,0 +1,14 @@
+package com.assignment;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SamMethodTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
